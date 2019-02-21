1200c1df80488e839a651a2618e4becb94ce867c
Aluno <Aluno@Digital.net>
2019-02-21 17:03:31 -0300

    aaaa

---
+++ src/ZeroTwo.js
@@ -0,0 +1,105 @@
+import 'phaser';
+
+var config = {
+        type: Phaser.AUTO,
+        scale: {
+            mode: Phaser.Scale.NONE,
+            _parent: 'WOW',
+            autoCenter: Phaser.Scale.CENTER_BOTH,
+            width: 800,
+            height: 600
+        },
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 300 },
+                debug: false
+            }
+        },
+        scene: {
+            preload: preload,
+            create: create,
+            update: update
+        }
+    };
+    
+    var game = new Phaser.Game(config);
+    var img;
+    var img2;
+    var vel = 300;
+    var scoreText;
+    var score = 0;
+    var tam = 0.1;
+
+    function preload ()
+    {
+        this.load.image('pic', 'assets/02.jpg');
+        this.load.image('heart', 'assets/heart.png');
+    }
+
+    function create ()
+    {     
+    //tem q ser um cursor pequeno para funcionar
+    //this.input.setDefaultCursor('url(assets/RedSword.png), pointer');
+
+    var sprite = this.add.sprite(0, 0, 'pic').setOrigin(0).setInteractive();
+
+    scoreText = this.add.text(16, 16, 'hearts: 0', { fontSize: '32px', fill: '#000' });
+
+    img = this.physics.add.sprite(100, 450, 'heart').setInteractive();
+    
+
+    img.setCollideWorldBounds(true);
+
+    this.input.on('pointerdown', function (pointer) {
+
+        img2 = this.physics.add.sprite(pointer.x, pointer.y, 'heart')
+        img2.setGravityY(300)
+        img2.setCollideWorldBounds(true)
+        img2.setBounce(0.2)
+        this.physics.add.collider(img, img2, vai, null, this);
+        
+
+    }, this);
+
+    sprite.on('pointerdown', function (pointer) {
+
+        this.setTint(0xD5A6BD);
+
+    });
+
+    sprite.on('pointerout', function (pointer) {
+
+        this.clearTint();
+
+    });
+
+    sprite.on('pointerup', function (pointer) {
+
+        this.clearTint();
+
+    });
+    }
+
+    function update ()
+    {   
+        
+        
+    }
+
+    function vai(){
+        tam += 0.1
+        if (tam > 5) {
+            tam = 1
+        }
+        vel = vel * -1
+
+        img.setVelocityX(vel)
+        img.setVelocityY(vel)
+
+        score += 1;
+        scoreText.setText('Score: ' + score);
+        img2.disableBody(true, true);
+        img.setScale(tam)
+    }
+
--- src/index.js
+++ src/index.js
@@ -24,82 +24,97 @@ var config = {
     };
     
     var game = new Phaser.Game(config);
-    var img;
-    var img2;
-    var vel = 300;
-    var scoreText;
+    var player;
+    var stars;
+    var bombs;
+    var platforms;
+    var cursors;
     var score = 0;
-    var tam = 0.1;
+    var gameOver = false;
+    var scoreText;
+    var keys;
 
     function preload ()
     {
-        this.load.image('pic', 'assets/02.jpg');
-        this.load.image('heart', 'assets/heart.png');
+        //cria um spritesheet com lateral de 32px e altura 48px
+        this.load.spritesheet('dude', 'assets/dude2.png', { frameWidth: 32, frameHeight: 48 })
+        this.load.image('back', 'assets/back.png')
+        
     }
 
     function create ()
     {     
-    //tem q ser um cursor pequeno para funcionar
-    //this.input.setDefaultCursor('url(assets/RedSword.png), pointer');
+        this.add.image(400, 350, 'back')
 
-    var sprite = this.add.sprite(0, 0, 'pic').setOrigin(0).setInteractive();
+        cursors = this.input.keyboard.createCursorKeys();
+        scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#FFF' });
 
-    scoreText = this.add.text(16, 16, 'hearts: 0', { fontSize: '32px', fill: '#000' });
+        player = this.physics.add.sprite(100, 450, 'dude');
+        player.body.setGravityY(300)
 
-    img = this.physics.add.sprite(100, 450, 'heart').setInteractive();
-    
+        //This means when it lands after jumping it will bounce ever so slightly.
+        player.setBounce(0.2);
 
-    img.setCollideWorldBounds(true);
+        player.setCollideWorldBounds(true);
 
-    this.input.on('pointerdown', function (pointer) {
+        this.anims.create({
+            //nome da animação = 'left'
+            key: 'left',
+            //define os frames dentro da image do sprite que serão utilizados no caso do primeiro até o terceiro
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            //The 'repeat -1' value tells the animation to loop.
+            repeat: -1
+        });
 
-        img2 = this.physics.add.sprite(pointer.x, pointer.y, 'heart')
-        img2.setGravityY(300)
-        img2.setCollideWorldBounds(true)
-        img2.setBounce(0.2)
-        this.physics.add.collider(img, img2, vai, null, this);
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'dude', frame: 4 } ],
+            frameRate: 20,
+            repeat: -1
+        });
         
-
-    }, this);
-
-    sprite.on('pointerdown', function (pointer) {
-
-        this.setTint(0xD5A6BD);
-
-    });
-
-    sprite.on('pointerout', function (pointer) {
-
-        this.clearTint();
-
-    });
-
-    sprite.on('pointerup', function (pointer) {
-
-        this.clearTint();
-
-    });
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
     }
 
     function update ()
     {   
-        
-        
-    }
-
-    function vai(){
-        tam += 0.1
-        if (tam > 5) {
-            tam = 1
+        if (cursors.left.isDown)
+        {   
+            //define a velocidade do player no eixo X
+            player.setVelocityX(-200);
+            //muda o sprite para o com o nome "left"
+            player.anims.play('left', true);
         }
-        vel = vel * -1
+        else if (cursors.right.isDown)
+        {
+            player.setVelocityX(200);
+        
+            player.anims.play('right', true);
+        }
+        else
+        {
+            //para o player
+            player.setVelocityX(0);
+        
+            player.anims.play('turn');
+        }
+        //se a seta para cima e estiver tocando o chão/algo
+        if (cursors.up.isDown)
+        {
+            //aplica a velocidade no eixo y, ou seja um pulo
+            player.setVelocityY(-500);
+        } 
 
-        img.setVelocityX(vel)
-        img.setVelocityY(vel)
-
-        score += 1;
-        scoreText.setText('Score: ' + score);
-        img2.disableBody(true, true);
-        img.setScale(tam)
+        if (cursors.down.isDown)
+        {
+            //aplica a velocidade no eixo y, ou seja um pulo
+            player.setVelocityY(500);
+        } 
+        
     }
-
